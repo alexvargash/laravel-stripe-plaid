dbfdc5115adff20683804085430589a68c86be93
Alex Vargas <alexvargas.dev@gmail.com>
2019-05-10 17:50:34 +0000

    Apply fixes from StyleCI

--- tests/StripePlaidTest.php
+++ tests/StripePlaidTest.php
@@ -6,9 +6,9 @@ use PHPUnit\Framework\TestCase;
 
 class StripePlaidTest extends TestCase
 {
-	/** @test */
-	public function it_returns_a_stripe_token()
-	{
-		$this->assertTrue(true);
-	}
+    /** @test */
+    public function it_returns_a_stripe_token()
+    {
+        $this->assertTrue(true);
+    }
 }

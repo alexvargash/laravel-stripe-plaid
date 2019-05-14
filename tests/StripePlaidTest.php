6f11b24bca157694e73df624b52ad4689ccfe541
Alex Vargas <avargas@digizent.com>
2019-05-14 10:15:31 -0500

    Added test for error on Plaid keys and expired token.

--- tests/StripePlaidTest.php
+++ tests/StripePlaidTest.php
@@ -4,9 +4,11 @@ namespace AlexVargash\LaravelStripePlaid\Tests;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Exception\RequestException;
 use AlexVargash\LaravelStripePlaid\StripePlaid;
 use AlexVargash\LaravelStripePlaid\Exceptions\PlaidException;
 
@@ -35,6 +37,56 @@ class StripePlaidTest extends TestCase
         $stripeToken = $stripePlaid->getStripeToken();
 
         $this->assertSame('btok_6IJuhsDmxnxDFFxYKGEnxc4a', $stripeToken);
+    }
+
+    /** @test */
+    public function the_public_token_should_not_be_expired()
+    {
+        $keys = [
+            'secret' => '5c74834hs3iag5as9884s1c8g9987d',
+            'client_id' => '1dd5243s03634t228712ss23',
+            'account_id' => 'aAdsDrJBeKvN43N9S2Y2UGhuXs2vd6JNsUCDH',
+            'public_token' => 'public-sandbox-8765c42w-2nd1-8976-432s-x37m6kjd78cu',
+            'environment' => 'sandbox',
+        ];
+
+        $mock = new MockHandler([
+            new Response(400, [], '{"display_message": null, "error_code": "INVALID_PUBLIC_TOKEN", "error_message": "provided public token is expired. Public tokens expire 30 minutes after creation at which point they can no longer be exchanged", "error_type": "INVALID_INPUT", "request_id": "wfn92ATB3EC83m5", "suggested_action": null }')
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        $this->expectException(PlaidException::class);
+        $this->expectExceptionMessage('{"display_message": null, "error_code": "INVALID_PUBLIC_TOKEN", "error_message": "provided public token is expired. Public tokens expire 30 minutes after creation at which point they can no longer be exchanged", "error_type": "INVALID_INPUT", "request_id": "wfn92ATB3EC83m5", "suggested_action": null }');
+
+        $stripePlaid = new StripePlaid($keys, $client);
+        $stripeToken = $stripePlaid->getStripeToken();
+    }
+
+    /** @test */
+    public function the_plaid_credentials_should_be_correct()
+    {
+        $keys = [
+            'secret' => '5c74834hs3iag5as9884s1c8g9987d',
+            'client_id' => '1dd5243s03634t228712ss23',
+            'account_id' => 'aAdsDrJBeKvN43N9S2Y2UGhuXs2vd6JNsUCDH',
+            'public_token' => 'public-sandbox-8765c42w-2nd1-8976-432s-x37m6kjd78cu',
+            'environment' => 'sandbox',
+        ];
+
+        $mock = new MockHandler([
+            new Response(400, [], '{"display_message": null, "error_code": "INVALID_API_KEYS", "error_message": "invalid client_id or secret provided", "error_type": "INVALID_INPUT", "request_id": "Qo8n3fkIGVrFj0j", "suggested_action": null }')
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        $this->expectException(PlaidException::class);
+        $this->expectExceptionMessage('{"display_message": null, "error_code": "INVALID_API_KEYS", "error_message": "invalid client_id or secret provided", "error_type": "INVALID_INPUT", "request_id": "Qo8n3fkIGVrFj0j", "suggested_action": null }');
+
+        $stripePlaid = new StripePlaid($keys, $client);
+        $stripeToken = $stripePlaid->getStripeToken();
     }
 
     /** @test */

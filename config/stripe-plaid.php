63fe982ef6827b0cf1f41809d8966205e098a357
Rogelio Bazh <rbazan@digizent.com>
2020-11-10 20:49:43 -0600

    Add config values

--- config/stripe-plaid.php
+++ config/stripe-plaid.php
@@ -39,4 +39,59 @@ return [
     |
     */
     'client_id' => env('PLAID_CLIENT_ID', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Client Name
+    |--------------------------------------------------------------------------
+    |
+    | The name of your application, as it should be displayed in Link.
+    | https://plaid.com/docs/#glossary
+    |
+    */
+    'client_name' => env('PLAID_CLIENT_NAME', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language
+    |--------------------------------------------------------------------------
+    |
+    | The language that Link should be displayed in.
+    | When using a Link customization, the language configured here must match the setting 
+    | in the customization, or the customization will not be applied.
+    | Supported languages are: English ('en'), French ('fr'), Spanish ('es'), Dutch ('nl')
+    | https://plaid.com/docs/#glossary
+    |
+    */
+    'language' => 'en',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Country Codes
+    |--------------------------------------------------------------------------
+    |
+    | Specify an array of Plaid-supported country codes using the ISO-3166-1 alpha-2 country code standard. 
+    | Note that if you initialize with a European country code, your users will see the European consent panel 
+    | during the Link flow. 
+    | If Link is launched with multiple country codes, only products that you are enabled for in all countries will be used by Link.
+    | Supported country codes are: US, CA, ES, FR, GB, IE, NL. Example value: ['US', 'CA']. 
+    | https://plaid.com/docs/#glossary
+    |
+    */
+    'country_codes' => ['US'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Products
+    |--------------------------------------------------------------------------
+    |
+    | List of Plaid product(s) you wish to use. If launching Link in update mode, 
+    | should be omitted; required otherwise 
+    | Supported products are: transactions, auth, identity, assets, investments, liabilities, payment_initiation. 
+    | Example value: ['auth', 'transactions'] 
+    | https://plaid.com/docs/#glossary
+    |
+    */
+    'products' => ['auth', 'transactions'],
+    
 ];
